0239761e8bb4d5e972cd261510578e0e40f6fe49
RitikAhirwar1 <ritikahirwar1020@gmail.com>
2025-04-06 16:06:21 +0530

    Update config.js

--- resume_frontend/src/api/config.js
+++ resume_frontend/src/api/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'https://resresume-production-e7b0.up.railway.appume-production-e7b0.up.railway.app';
+const API_BASE_URL = "https://resume-production-e7b0.up.railway.app"
 
 export default API_BASE_URL;
